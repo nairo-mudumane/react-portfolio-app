01e304c1caca0cb442e464b8a42161db29cda890
nairo-mudumane <nazirmudumane@gmail.com>
2022-02-03 17:01:25 +0200

    created global styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import GlobalStyles from "./global/globalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <HashRouter>
       <App />
     </HashRouter>
