c68ea02b19ba599e89244bf6c244e93e817288b7
nairo-mudumane <nazirmudumane@gmail.com>
2022-03-01 05:27:20 +0200

    smooth scroll not working

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { ScrollableData } from "./components";
 import { GlobalStyles } from "./global";
 
 function App() {
     return (
         <>
             <GlobalStyles />
+            <ScrollableData>
+                <div style={{ height: "200vh" }}>
+                    <p>aoisfoai</p>
+                </div>
+            </ScrollableData>
         </>
     );
 }
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as ScrollableData } from "./Scrollable-data";
