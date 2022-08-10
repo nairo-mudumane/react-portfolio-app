46d247bd99f1e889a3a1a5128703f10ef5e09589
nairo-mudumane <nairomudumane@outlook.com>
2022-08-10 15:54:58 +0200

    updated header

--- src/components/Header/config.ts
+++ src/components/Header/config.ts
@@ -3,4 +3,16 @@ export const PAGES = [
         label: "Home",
         link: "/",
     },
+    {
+        label: "About",
+        link: "/about",
+    },
+    {
+        label: "Works",
+        link: "/works",
+    },
+    {
+        label: "Contact",
+        link: "/contacts",
+    },
 ];
